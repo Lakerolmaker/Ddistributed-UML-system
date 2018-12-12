af32dd4efb7830109465a5de2edbd31e58ca248b
VicRoman97 <38137876+VicRoman97@users.noreply.github.com>
2018-12-12 13:25:07 +0100

    Update StandardValues.java
    fixed

--- Visualization/src/MainPackage/StandardValues.java
+++ Visualization/src/MainPackage/StandardValues.java
@@ -4,19 +4,29 @@ import javafx.scene.text.Font;
 
 public class StandardValues {
 
+	//: standard height and width of the classes
 	public static double width = 400;
 	public static double height = 100;
 
+	//: some padding inside the classes
 	double linePadding = 10;
 	double paddingX = 50;
 	double paddingY = 50;
 	
+	//: limit of a single image being rendered
+	public static double normalredering_limit = 1000;
+	
 	//: Padding between classes,  both X and Y
 	public static double padding = 100;
+	
+	//: Width of the line connecting classes
+	public static double arrowWidth = 5;
 
+	//: Padding of the canvas and the edge of the canvas
 	public static double canvasPadding_X = 0;
 	public static double canvasPadding_Y = 100;
 
+	//: standard font of the classes
 	public static Font font = new Font("Arial", 20);
 
 }
